49233feff251491683c5ada49fe73f89a6521579
pavani-pathirana <pavani997work@gmail.com>
2024-10-18 18:24:02 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace RockPaperScissorsWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(292, 120);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(75, 23);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "ROCK";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(218, 158);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(75, 23);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "PAPER";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Location = new System.Drawing.Point(356, 158);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(75, 23);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "SCISSORS";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(475, 30);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(163, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Choose Rock, Paper, or Scissors";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(131, 30);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(62, 13);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "Score: 0 - 0";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(292, 199);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissorsWinForms
+{
+
+    
+    public partial class Form1 : Form
+    {
+        private int playerScore = 0;
+        private int computerScore = 0;
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            PlayGame("rock");
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            PlayGame("paper");
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            PlayGame("scissors");
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            playerScore = 0;
+            computerScore = 0;
+            UpdateScore();
+            lblResult.Text = "Choose Rock, Paper, or Scissors!";
+        }
+
+        private void PlayGame(string playerChoice)
+        {
+            string[] choices = { "rock", "paper", "scissors" };
+            string computerChoice = choices[random.Next(choices.Length)];
+
+            string result = DetermineWinner(playerChoice, computerChoice);
+            lblResult.Text = $"You chose {playerChoice}, Computer chose {computerChoice}. {result}";
+
+            UpdateScore();
+        }
+
+        private string DetermineWinner(string player, string computer)
+        {
+            if (player == computer)
+                return "It's a tie!";
+
+            if ((player == "rock" && computer == "scissors") ||
+                (player == "scissors" && computer == "paper") ||
+                (player == "paper" && computer == "rock"))
+            {
+                playerScore++;
+                return "You win!";
+            }
+            else
+            {
+                computerScore++;
+                return "You lose!";
+            }
+        }
+
+        private void UpdateScore()
+        {
+            lblScore.Text = $"Score: {playerScore} - {computerScore}";
+        }
+    }
+}
